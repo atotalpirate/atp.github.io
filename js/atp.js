f0faf2605208625cf1f5aeb06cbc064645a5f10f
atotalpirate <jnholway@gmail.com>
2014-03-30 21:59:56 -0500

    sunday funday

--- js/atp.js
+++ js/atp.js
@@ -1,19 +1,42 @@
+$(document).ready(function(){
+
 // Header style change on scroll
 
-var vh = $(window).height();
+// var vh = $(window).height();
+  var intro = $("#intro").height();
+  var declaration = $("#declaration").height();
 
-$(function(){
- var shrinkHeader = vh / 2;
-  $(window).scroll(function() {
-    var scroll = getCurrentScroll();
-      if ( scroll >= shrinkHeader ) {
-           $('.header').addClass('shrink');
-        }
-        else {
-            $('.header').removeClass('shrink');
-        }
+  $(function(){
+   var changeHeader = intro - 20;
+    $(window).scroll(function() {
+      var scroll = getCurrentScroll();
+        if ( scroll >= changeHeader ) {
+             $('.header').addClass('dark');
+          }
+          else {
+              $('.header').removeClass('dark');
+          }
+    });
+  function getCurrentScroll() {
+      return window.pageYOffset;
+      }
   });
-function getCurrentScroll() {
-    return window.pageYOffset;
-    }
+
+  $(function(){
+   var changeHeaderAgain = intro + declaration;
+    $(window).scroll(function() {
+      var scroll = getCurrentScroll();
+        if ( scroll >= changeHeaderAgain ) {
+             $('.header').removeClass('dark');
+             $('.header').addClass('darkred');
+          }
+          else {
+              $('.header').removeClass('darkred');
+          }
+    });
+  function getCurrentScroll() {
+      return window.pageYOffset;
+      }
+  });
+
 });
