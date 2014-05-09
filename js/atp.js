5d1a00a8dd2365de88369e6cb5f571ebf169e846
atotalpirate <jnholway@gmail.com>
2014-05-09 16:20:18 -0600

    Added svgs, better interactivity animations and front page js

--- js/atp.js
+++ js/atp.js
@@ -1,26 +1,37 @@
 $(document).ready(function(){
 
 // Header style change on scroll
+var lastScrollTop = 0, delta = 5;
+ $(window).scroll(function(event){
+    var st = $(this).scrollTop();
 
-// var vh = $(window).height();
-  var intro = $("#intro").height();
-  var declaration = $("#declaration").height();
+    if(Math.abs(lastScrollTop - st) <= delta)
 
-  $(function(){
-   var changeHeader = intro - 20;
-    $(window).scroll(function() {
-      var scroll = getCurrentScroll();
-        if ( scroll >= changeHeader ) {
-             $('.header').addClass('dark');
-          }
-          else {
-              $('.header').removeClass('dark');
-          }
-    });
-  function getCurrentScroll() {
-      return window.pageYOffset;
-      }
+
+    if (st > lastScrollTop){
+        // downscroll code
+        $( "#desktopmenuwrap" ).addClass("scrolldown").removeClass("scrollup");
+    } else {
+       // upscroll code
+         $( "#desktopmenuwrap" ).addClass("scrollup").removeClass("scrolldown");
+    }
+    lastScrollTop = st;
+ });
+
+ var item = $(".trio div");
+   item.click(function() {
+
+    var trioItem = $(".trioitem");
+    var trioAdjacent = $(".trioitem + .triospan");
+
+     if(trioItem.hasClass("active") || trioAdjacent.hasClass("active")){
+       trioItem.removeClass("active");
+       trioAdjacent.removeClass("active");
+     }
+
+    $(this).addClass("active");
+    $(this).next().addClass("active");
+
   });
-
 
 });
