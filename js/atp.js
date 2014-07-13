c28c0cd1ab80ab7e56171ca76c04c8061b1f0de7
atotalpirate <jnholway@gmail.com>
2014-07-12 20:35:33 -0700

    removed unused js from footer and refactored front page clicker jquery

--- js/atp.js
+++ js/atp.js
@@ -1,21 +1,20 @@
- $(document).ready(function(){
+ $(function(){
 
+  var $introClick, $introWrap;
 
-  var itemIntro = $(".introclick");
-    itemIntro.click(function() {
+  $introClick = $('.introclick');
+  $introWrap = $('.introwrap');
 
-     var trioItem = $(".introclick");
-     var trioAdjacent = $(".introclick .introwrap");
+  //Add event listener to clicked icon
+  $introClick.on('click', function(){
+    //remove active attribute on all siblings
+    $(this).siblings().removeClass('active');
+    //add active attribute to clicked icon and the content container next to it
+    $(this).addClass('active');
+    $(this).next().addClass('active');
+  });
 
-      if(trioItem.hasClass("active") || trioAdjacent.hasClass("active")){
-        trioItem.removeClass("active");
-        trioAdjacent.removeClass("active");
-      }
-
-     $(this).addClass("active");
-     $(this).next().addClass("active");
-
-   });
+  //Remove active attribute from adjacent elements
 
  var itemMid = $(".trio div");
    itemMid.click(function() {
