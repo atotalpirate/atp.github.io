8731558982eeab1df2c64475a2bed0d07869343a
atotalpirate <jnholway@gmail.com>
2014-07-15 16:24:19 -0700

    rewrote the front page tools section to utilize AJAX and a JSON file

--- js/atp.js
+++ js/atp.js
@@ -1,9 +1,10 @@
- $(function(){
+//jQuery ready state function in short hand
+$(function(){
+  //Declare variables
+  var $introClick;
 
-  var $introClick, $introWrap;
-
+  //Cache click event locations
   $introClick = $('.introclick');
-  $introWrap = $('.introwrap');
 
   //Add event listener to clicked icon
   $introClick.on('click', function(){
@@ -14,15 +15,63 @@
     $(this).next().addClass('active');
   });
 
-  //Remove active attribute from adjacent elements
+  //Declare icons global variable
+  var icons;
+  //Setup request
+  $.ajax({
+    //Before requesting data
+    beforeSend: function(xhr){
+      //If supported, set MIME to prevent errors
+      if (xhr.overrideMimeType){
+        xhr.overrideMimeType("application/json");
+      }
+    }
+  });
 
- var itemMid = $('.trio div');
-   itemMid.on("click", function(){
-     //remove active class attribute from siblings
-     $(this).siblings().removeClass('active');
-     //add active class attribute to clicked group
-     $(this).addClass('active');
-     //traverse and add active class attribute
-     $(this).next().addClass('active');
-   });
+  //Function that collects data from the json file
+  function loadIconInfo(){
+    //Getting JSON file
+    $.getJSON('js/front.json')
+    //If successful, cache json data into icons variable
+    .done(function(data){
+      icons = data;
+    //If there's a problem, display an error message
+    }).fail(function(){
+      var errorMessage = document.createElement('div').addClass('error').html('<em>SHIT!</em> Something went wrong. I\'m working on it.' );
+      $('#middle').append(errorMessage);
+    });
+
+  }
+
+  //Fire loadIconInfo();
+  loadIconInfo();
+
+  //Create on click event handler for front page icons
+  $('.trioitem').on('click', function(){
+
+    //Declare variables
+    var newContent;
+    //Get and cache ID attribute of clicked icon for use in looping
+    var ID = this.id.toUpperCase();
+
+    //Declare empty variable for creating new elements
+    newContent = ' ';
+    //Loop through JSON data to create new elements
+    for (var i = 0; i < icons[ID].length; i++) {
+
+      newContent += '<div class="thing ' + icons[ID][i].title + '">';
+      newContent += '<img class="logo" src="' + icons[ID][i].logo + '"/>';
+      newContent += '</div>';
+
+    }
+    
+    //Find existing JSON data and remove it
+    $('.toolswrap').remove();
+    //Build markup that hosts data drawn from JSON via ajax and inject newContent
+    $(".trio").after('<div class="toolswrap">' + newContent + '</div>');
+
+    //Remove active class attribute from other icons
+    $('.trio .active').removeClass('active');
+    $(this).addClass('active');
+  });
 });
